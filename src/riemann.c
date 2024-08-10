14e80efcf38c1cbf0755239fad9c55e39b08e9d7
Francisco Castillo <122114861+FranzCastillo@users.noreply.github.com>
2024-08-10 01:33:15 -0600

    Ejercicio 4 + Fixed Decimal Precision

--- src/riemann.c
+++ src/riemann.c
@@ -2,34 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 
-const float n = 10e6;
+const double n = 10e6;
 
-float f1(const float x) {
-    return x * x;
-}
-
-float f2(const float x) {
-    return 2 * pow(x, 3);
-}
-
-float f3(const float x) {
+double f(const double x) {
+    // return x * x;
+    // return 2 * pow(x, 3);
     return sin(x);
 }
 
 /**
 * Compute the integral of f(x) using Rieman's method with the left rule
 */
-float trapezoides(const float dx, const int a, const int b) {
-    float sum = 0;
+double trapezoides(const double dx, const int a, const int b) {
+    double sum = 0;
     for (int i = 0; i < n; i++) {
-        const float x = a + i * dx;
-        sum += f3(x);
+        const double x = a + i * dx;
+        sum += f(x);
     }
     return sum * dx;
 }
 
 int main(int argc, char *argv[]) {
-    // Number of threads must be provided as a command-line argument
     if (argc != 3) {
         printf("Usage: %s <lower_limit (a)> <upper_limit (b)>\n", argv[0]);
         return 1;
@@ -39,7 +32,7 @@ int main(int argc, char *argv[]) {
     const int a = atoi(argv[1]);
     const int b = atoi(argv[2]);
 
-    const float dx = (b - a) / n;
+    const double dx = (b - a) / n;
 
     printf("n = %e\n", n);
     printf("f(x) = sin(x)\n");
